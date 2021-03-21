e78110657bbffff1d280549e73bf19cab10b9b13
Tucker Barton <hisashiburix@gmail.com>
2021-03-20 20:56:56 -0400

    Update TODO TASKS/LIST again.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import {createProjectForm, createProject} from "./project.js"
 //     c. Todo Item property input boxes aren't clickable for half of the box
 // 2. Visual Overhaul to mimic other todolist apps
 // 3. Save input to local storage so that data persists upon multiple visits
+// 4. Restructure code to make it less messy, esp. around event listeners
+// 5. Have ability to delete projects, lists, and todos
+// 6. Have ability to update projects, lists, and todos
+
 
 (function clickListener() {
     // Creates form for new project once "Create a Project!" button is clicked
